e75df7a25b9a6b98607b9e5cd28d4e8a8218a0cc
pele solomon <pelesolomon12@gmail.com>
2026-05-16 01:03:37 +0300

    Add loading screens (desktop + mobile) with animation

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,3 +3,6 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// הסר מסך טעינה לאחר שה-React נטען
+setTimeout(() => (window as any).__hideSplash?.(), 800);
